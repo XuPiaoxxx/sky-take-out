c3db130f3da5e334e04d218998c4b824d038e3b6
Xupaioxxx <2693798300@qq.com>
2023-08-07 16:50:42 +0800

    DAY2新增员工业务代码

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.sky.controller.admin;
 
 import com.sky.constant.JwtClaimsConstant;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
@@ -72,8 +73,15 @@ public class EmployeeController {
      */
     @PostMapping("/logout")
     @ApiOperation(value = "员工退出")
-    public Result<String> logout() {
+    public Result<String> logout() {return Result.success();}
+
+    //json格式数据，加注解@RequsetBody
+    @PostMapping
+    @ApiOperation("新增员工")
+    public Result save(@RequestBody EmployeeDTO employeeDTO){
+        log.info("新增员工: {}",employeeDTO);
+        System.out.println("当前线程的id:"+Thread.currentThread().getId());
+        employeeService.save(employeeDTO);
         return Result.success();
     }
-
 }
